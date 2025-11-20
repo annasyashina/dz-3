89211e6e9a69289ebd742fc4dab9b31c6584bb79
sowa <sowa84@bk.ru>
2025-11-20 15:41:00 +0400

    Решение ДЗ работа с ОС добавление проверки расширения файла

--- file/file.go
+++ file/file.go
@@ -1,31 +1,43 @@
 package file
 
 import (
+	"errors"
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 func ReadFile(name string) ([]byte, error) {
-	data, err := os.ReadFile(name)
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
+	ext := filepath.Ext(name)
+	if ext == "json" {
+		data, err := os.ReadFile(name)
+		if err != nil {
+			fmt.Println(err)
+			return nil, err
+		}
+		fmt.Println(string(data))
+		return data, nil
 	}
-	fmt.Println(string(data))
-	return data, nil
+	return nil, errors.New("Файл не является файлом json")
+
 }
 
 func WriteFile(content []byte, name string) {
-	file, err := os.Create(name)
-	if err != nil {
-		fmt.Println(err)
-	}
-	_, err2 := file.Write(content)
-	if err2 != nil {
-		defer file.Close()
-		fmt.Println(err2)
-		return
-	}
+	ext := filepath.Ext(name)
+	if ext == "json" {
+		file, err := os.Create(name)
+		if err != nil {
+			fmt.Println(err)
+		}
+		_, err2 := file.Write(content)
+		if err2 != nil {
+			defer file.Close()
+			fmt.Println(err2)
+			return
+		}
 
-	fmt.Println("Запись успешна")
+		fmt.Println("Запись успешна")
+	} else {
+		fmt.Println("Файл не является файлом json")
+	}
 }
